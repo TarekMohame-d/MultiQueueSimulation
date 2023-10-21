e46ac9c4653c4632cac17e49b74238c93c0c9d0b
Tarek Mohamed <tarekmohamed1019@gmail.com>
2023-10-21 21:40:18 +0300

    clsulation file

--- MultiQueueModels/CalculationsHelper.cs
+++ MultiQueueModels/CalculationsHelper.cs
@@ -8,6 +8,27 @@ namespace MultiQueueModels
 {
     public class CalculationsHelper
     {
-        
+        void timeDistributions(int serverID)
+        {
+            RawData rawData = ExtraxtData.rawData;
+            List<double> cumulativeProbability = new List<double>();
+            cumulativeProbability[0] = rawData.serviceDistributions[serverID].serverProbability[0] + 0;
+            for (int i = 1; i < rawData.serviceDistributions[serverID].serverProbability.Count; i++)
+            {
+                cumulativeProbability[i] = rawData.serviceDistributions[serverID].serverProbability[i-1] +
+                    rawData.serviceDistributions[serverID].serverProbability[i];
+            }
+            for (int i = 0; i < rawData.serviceDistributions[serverID].serverTime.Count;i++)
+            {
+                 
+                /*TimeDistribution timeDistribution = new TimeDistribution(
+                    rawData.serviceDistributions[serverID].serverTime[i],
+                    rawData.serviceDistributions[serverID].serverProbability[i],
+                    cumulativeProbability[i],
+
+                    );*/
+            }    
+        }
+
     }
 }
